a5de2a1a43ac667f8099b2bcff1bb262ae2fe656
moraxs <2237670756@qq.com>
2023-12-03 10:59:45 +0800

    add

--- src/pages/start/components/monster.vue
+++ src/pages/start/components/monster.vue
@@ -1,23 +1,58 @@
+<template>
+  <div>
+    <!-- 滚动公告 -->
+    <div class="scrolling-notice" v-if="showNotice">
+      <marquee behavior="scroll" direction="left">{{ noticeContent }}</marquee>
+    </div>
+
+    <!-- 原有的组件内容 -->
+    <div class="commuse">
+      
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 怪物: </div>
+        <a-cascader allow-search v-model="value2" :options="options" placeholder="" filterable />
+      </div>
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 等级: </div>
+        <a-input-number v-model="grade" placeholder="请输入数量" mode="button" size="large" class="input-demo" />
+      </div>
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 数量: </div>
+        <a-input-number v-model="num" placeholder="请输入数量" mode="button" size="large" class="input-demo" />
+      </div>
+
+      <div class="generate">
+        <a-input v-model="value" placeholder="" />
+        <a-button type="outline" @click="copyvalue">复制</a-button>
+        <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
-import { reactive, ref, computed } from 'vue'
+import { reactive, ref, computed, inject, onMounted } from 'vue'
 import { useClipboard } from '@vueuse/core'
 import { Message } from '@arco-design/web-vue'
+import monster from './json/monster.json'
 import { useAppStore } from '@/store/modules/app'
 
-import monster from './json/monster.json'
 const { text, isSupported, copy } = useClipboard()
 const appStore = useAppStore()
 
 var value2 = ref(1004010)
 var grade = ref(80)
 var num = ref(1)
-var uid = ref('')
 
 const value = computed(() => {
   return `/spawn ${value2.value} ${num.value} ${grade.value}`
 })
+
 const options = reactive(monster)
+
 const message = Message
 
 function copyvalue() {
@@ -26,35 +61,33 @@ function copyvalue() {
     message.success(`已复制${value.value}`)
   }
 }
+
 const send: any = inject("send")
+
+const showNotice = ref(true)
+const noticeContent = 'LunarCore及其他任何衍生工具都是免费软件，如果你是付费购买的，那你就被骗了，请及时退款并举报。'
+
+// 在页面加载时设置一个延时，用于显示滚动公告，你可以根据需求调整延时时长
+onMounted(() => {
+  setTimeout(() => {
+    showNotice.value = true
+  }, 1000)
+})
 </script>
 
-<template>
-  <div class="commuse">
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> UID: </div>
-      <a-input v-model="uid" placeholder="请输入UID" allow-clear />
-    </div>
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 怪物: </div>
-      <a-cascader allow-search v-model="value2" :options="options" placeholder="" filterable />
-    </div>
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 等级: </div>
-      <a-input-number v-model="grade" placeholder="请输入数量" mode="button" size="large" class="input-demo" />
-    </div>
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 数量: </div>
-      <a-input-number v-model="num" placeholder="请输入数量" mode="button" size="large" class="input-demo" />
-    </div>
-    <div class="generate">
-      <a-input v-model="value" placeholder="" />
-      <a-button type="outline" @click="copyvalue">复制</a-button>
-      <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
-    </div>
-  </div>
-</template>
 <style lang="less" scoped>
+/* 添加样式以美化滚动公告 */
+.scrolling-notice {
+  background-color: #ff6464;
+  color: #333;
+  padding: 8px;
+  font-size: 14px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  border-radius: 10px; /* 添加圆角样式 */
+}
+
 .commuse {
   width: 500px;
   margin: auto;
