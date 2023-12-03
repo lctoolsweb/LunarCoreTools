bd9b055c3ed6029b01518b04959780d9462b2ae9
moraxs <2237670756@qq.com>
2023-12-03 10:12:42 +0800

    fix

--- src/pages/start/components/avatar.vue
+++ src/pages/start/components/avatar.vue
@@ -60,7 +60,7 @@ function copyvalue() {
 const send = inject("send")
 
 const showNotice = ref(true)
-const noticeContent = '本工具免费，如果你是付费购买的，请举报'
+const noticeContent = 'LunarCore及其他任何衍生工具都是免费软件，如果你是付费购买的，那你就被骗了，请及时退款并举报。'
 
 // 在页面加载时设置一个延时，用于显示滚动公告，你可以根据需求调整延时时长
 onMounted(() => {
