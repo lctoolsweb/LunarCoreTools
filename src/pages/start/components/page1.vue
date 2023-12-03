d1c438de913a3a11a05c5ca7a48f90e3510b3318
moraxs <2237670756@qq.com>
2023-12-03 11:38:10 +0800

    awa

--- src/pages/start/components/page1.vue
+++ src/pages/start/components/page1.vue
@@ -1,9 +1,34 @@
+<template>
+  <div>
+    <!-- 滚动公告 -->
+    <div class="scrolling-notice" v-if="showNotice">
+      <marquee behavior="scroll" direction="left">{{ noticeContent }}</marquee>
+    </div>
+
+    <!-- 原有的组件内容 -->
+    <div class="commuse">
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 头像: </div>
+        <a-select allow-search v-model="value2" :options="options" placeholder="请输入物品" filterable />
+      </div>
+
+      <div class="generate">
+        <a-input v-model="value" placeholder="" />
+        <a-button type="outline" @click="copyvalue">复制</a-button>
+        <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import { reactive, ref, computed } from 'vue'
+import { reactive, ref, computed, inject, onMounted } from 'vue'
 import { useClipboard } from '@vueuse/core'
+import { Message } from '@arco-design/web-vue'
 import page1 from './json/head.json'
 import { useAppStore } from '@/store/modules/app'
-import { Message } from '@arco-design/web-vue'
+
 const { text, isSupported, copy } = useClipboard()
 const appStore = useAppStore()
 
@@ -13,8 +38,8 @@ var value3 = ref('/give')
 const value = computed(() => {
   return `${value3.value} ${value2.value}`
 })
-const options = reactive(page1)
 
+const options = reactive(page1)
 
 const message = Message
 
@@ -24,26 +49,33 @@ function copyvalue() {
     message.success(`已复制${value.value}`)
   }
 }
+
 const send: any = inject("send")
+
+const showNotice = ref(true)
+const noticeContent = 'LunarCore及其他任何衍生工具都是免费软件，如果你是付费购买的，那你就被骗了，请及时退款并举报。'
+
+// 在页面加载时设置一个延时，用于显示滚动公告，你可以根据需求调整延时时长
+onMounted(() => {
+  setTimeout(() => {
+    showNotice.value = true
+  }, 1000)
+})
 </script>
 
-<template>
-  <div class="commuse">
-
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 头像: </div>
-      <a-select allow-search v-model="value2" :options="options" placeholder="请输入物品" filterable />
-    </div>
-
-  
-    <div class="generate">
-      <a-input v-model="value" placeholder="" />
-      <a-button type="outline" @click="copyvalue">复制</a-button>
-      <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
-    </div>
-  </div>
-</template>
 <style lang="less" scoped>
+/* 添加样式以美化滚动公告 */
+.scrolling-notice {
+  background-color: #ff6464;
+  color: #333;
+  padding: 8px;
+  font-size: 14px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  border-radius: 10px; /* 添加圆角样式 */
+}
+
 .commuse {
   width: 500px;
   margin: auto;
@@ -55,7 +87,7 @@ const send: any = inject("send")
   color: #000;
   margin: 18px 0;
 
-  >div {
+  > div {
     &:nth-child(1) {
       width: 150px;
       text-align: right;
