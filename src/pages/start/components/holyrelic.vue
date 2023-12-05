56dbb53162293ad9fd2c8c6eb3322a7b137a03c5
HongchengQ <q200608233416@163.com>
2023-12-05 19:49:44 +0800

    为遗器页面添加滚动弹窗

--- src/pages/start/components/holyrelic.vue
+++ src/pages/start/components/holyrelic.vue
@@ -1,3 +1,53 @@
+<template>
+  <div>
+    <!-- 滚动公告 -->
+    <div class="scrolling-notice" v-if="showNotice">
+      <marquee behavior="scroll" direction="left">{{ noticeContent }}</marquee>
+    </div>
+
+    <!-- 原有的组件内容 -->
+    <div class="commuse">
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 遗器: </div>
+        <a-cascader allow-search v-model="holyrelicnamevalue" :options="options" placeholder="请输入物品" filterable />
+      </div>
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 主属性: </div>
+        <a-cascader allow-search v-model="holyrelicnmainvalue" :options="options2" placeholder="请输入主属性" filterable />
+        <!-- <n-select v-model:value="holyrelicnmainvalue" filterable placeholder="选择遗器主属性" :options="options2" /> -->
+      </div>
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 属性小词条: </div>
+
+        <div class="smallho">
+          <div class="smallho-item" v-for="(item, index) in options3" :key="index">
+            <a-checkbox v-model="item.isCheck"></a-checkbox>
+            <div class="text-slate-900 dark:text-slate-100">{{ item.label }} </div>
+            <div>
+              <a-input-number placeholder="" v-model="item.num" :min="1" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="commuse-item">
+        <div class="text-slate-900 dark:text-slate-100"> 强化等级: </div>
+
+        <a-input-number placeholder="" v-model="grade" :min="0" :max="20" />
+      </div>
+
+      <div class="generate">
+        <a-input v-model="value" placeholder="" />
+        <a-button type="outline" @click="copyvalue">复制</a-button>
+        <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
 
 <script setup lang="ts">
 import { reactive, ref, computed } from 'vue'
@@ -54,50 +104,29 @@ function copyvalue() {
   }
 }
 const send: any = inject("send")
+const showNotice = ref(true)
+const noticeContent = 'LunarCore及其他任何衍生工具都是免费软件，如果你是付费购买的，那你就被骗了，请及时退款并举报。'
+
+// 在页面加载时设置一个延时，用于显示滚动公告，你可以根据需求调整延时时长
+onMounted(() => {
+  setTimeout(() => {
+    showNotice.value = true
+  }, 1000)
+})
+
 </script>
 
-<template>
-  <div class="commuse">
-
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 遗器: </div>
-      <a-cascader allow-search v-model="holyrelicnamevalue" :options="options" placeholder="请输入物品" filterable />
-    </div>
-
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 主属性: </div>
-      <a-cascader allow-search v-model="holyrelicnmainvalue" :options="options2" placeholder="请输入主属性" filterable />
-      <!-- <n-select v-model:value="holyrelicnmainvalue" filterable placeholder="选择遗器主属性" :options="options2" /> -->
-    </div>
-
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 属性小词条: </div>
-
-      <div class="smallho">
-        <div class="smallho-item" v-for="(item, index) in options3" :key="index">
-          <a-checkbox v-model="item.isCheck"></a-checkbox>
-          <div class="text-slate-900 dark:text-slate-100">{{ item.label }} </div>
-          <div>
-            <a-input-number placeholder="" v-model="item.num" :min="1" />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="commuse-item">
-      <div class="text-slate-900 dark:text-slate-100"> 强化等级: </div>
-
-      <a-input-number placeholder="" v-model="grade" :min="0" :max="20" />
-    </div>
-
-    <div class="generate">
-      <a-input v-model="value" placeholder="" />
-      <a-button type="outline" @click="copyvalue">复制</a-button>
-      <a-button type="outline" v-if="appStore.isLogin" @click="send(value)">执行</a-button>
-    </div>
-  </div>
-</template>
 <style lang="less" scoped>
+/* 添加样式以美化滚动公告 */
+.scrolling-notice {
+  color: #BEBEBE;
+  padding: 8px;
+  font-size: 14px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
 .commuse {
   width: 500px;
   margin: auto;
